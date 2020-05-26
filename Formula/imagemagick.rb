43e0be8fab58a5f02b49dee5a7ffa2a42db3e9ba
Thomas L. Kjeldsen <tlk@users.noreply.github.com>
2020-05-26 16:16:51 +0200

    Update imagemagick.rb

--- Formula/imagemagick.rb
+++ Formula/imagemagick.rb
@@ -6,12 +6,6 @@ class Imagemagick < Formula
   sha256 "f7abbc65cce01caa314ef4cc9acf8adfa7b633689901a52f29d681230a997c59"
   head "https://github.com/ImageMagick/ImageMagick.git"
 
-  bottle do
-    sha256 "bd188f22ea4373a97496a1f469d02bc72c6ff38f0b74c9fba7f5ad480d431b24" => :catalina
-    sha256 "575bfca45640c48a9228e8be95c3606b813faa31ad00aad7795e1a1de7d4fb59" => :mojave
-    sha256 "96207139743ea91a5ce433a8a2d441376008eae5d18827cfb9aa21f5fea44829" => :high_sierra
-  end
-
   depends_on "pkg-config" => :build
   depends_on "freetype"
   depends_on "ghostscript"
