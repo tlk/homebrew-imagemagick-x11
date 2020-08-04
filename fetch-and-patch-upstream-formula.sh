229ed7aad4a69c3aa07b7f4c4855566025f08263
Thomas L. Kjeldsen <tlk@closureconsulting.com>
2020-08-04 18:11:20 +0200

    Update the formula once a week

---
+++ fetch-and-patch-upstream-formula.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+FORMULA=Formula/imagemagick.rb
+
+wget -q -O - https://raw.githubusercontent.com/Homebrew/homebrew-core/master/Formula/imagemagick.rb \
+    | sed 's/manipulate images in many formats"/manipulate images in many formats (X11 support)"/g' \
+    | sed '/  bottle do/,/  end/d' \
+    > $FORMULA
+
+patch $FORMULA imagemagick-x11.patch
+
+git commit \
+    -m "Merge upstream" \
+    -m "" \
+    -m "Source https://raw.githubusercontent.com/Homebrew/homebrew-core/master/Formula/imagemagick.rb" \
+    $FORMULA
+
