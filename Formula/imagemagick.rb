76bb7f047ab39c59095708edc3328b87d373101d
Thomas L. Kjeldsen <tlk@closureconsulting.com>
2021-08-26 19:35:16 +0200

    Interrim: Merge upstream but without X11 patch

--- Formula/imagemagick.rb
+++ Formula/imagemagick.rb
@@ -1,10 +1,9 @@
 class Imagemagick < Formula
   desc "Tools and libraries to manipulate images in many formats (X11 support)"
   homepage "https://www.imagemagick.org/"
-  url "https://www.imagemagick.org/download/releases/ImageMagick-7.1.0-4.tar.xz"
-  sha256 "1a54bd46947f16fb29cf083be3614a14135f2fe9d1aa20665a85a8940bf6dc65"
+  url "https://www.imagemagick.org/download/releases/ImageMagick-7.1.0-5.tar.xz"
+  sha256 "faeae351785447834c44850f2d3d80bbb0fed500228373098fce553a0df1ac6b"
   license "ImageMagick"
-  revision 2
   head "https://github.com/ImageMagick/ImageMagick.git", branch: "main"
 
   livecheck do
@@ -33,80 +32,46 @@ class Imagemagick < Formula
   uses_from_macos "libxml2"
   uses_from_macos "zlib"
 
-  depends_on "libx11"
-  depends_on "graphviz" => :optional
+  on_linux do
+    depends_on "libx11"
+  end
 
   skip_clean :la
-
-
-  # experimental patch, see details at the bottom of this file
-  patch :DATA
-
-  # Alternative to the patch. Adding these dependencies allows the project to
-  # build but the `display wizard` segfaults.
-  #
-  # depends_on "libxext"
-  # depends_on "libsm"
-  # depends_on "libxt"
-
 
   def install
     # Avoid references to shim
     inreplace Dir["**/*-config.in"], "@PKG_CONFIG@", Formula["pkg-config"].opt_bin/"pkg-config"
 
-
-    # Add a symlink that points to the X11 include files provided by XQuartz.
-    # This prevents the `display wizard` command from segfaulting.
-    ln_s "/opt/X11/include/X11", "#{buildpath}/X11"
-
-
-    # Adding -I/opt/X11/include and -L/opt/X11/lib via compiler flags is blocked
-    # by the HOMEBREW_CCCFG=Osa setting. (=sa works, maybe)
-    #
-    # See the following for details:
-    #   https://github.com/Homebrew/brew/blob/master/Library/Homebrew/extend/ENV/super.rb#L83-L88
-    #   https://github.com/Homebrew/brew/blob/master/Library/Homebrew/shims/super/cc#L36
-    #   https://github.com/Homebrew/brew/blob/master/Library/Homebrew/shims/super/cc#L108
-    #   https://github.com/Homebrew/brew/blob/master/Library/Homebrew/shims/super/cc#L140
-    #   https://github.com/Homebrew/brew/blob/master/Library/Homebrew/shims/super/cc#L171       <-- refurbish_arg,
-    #   https://github.com/Homebrew/brew/blob/master/Library/Homebrew/shims/super/cc#L252-L254      /opt/X11 is being filtered out
-    #   https://github.com/Homebrew/brew/blob/master/Library/Homebrew/shims/super/cc#L270
-    #   https://github.com/Homebrew/brew/blob/master/Library/Homebrew/shims/super/cc#L357
-
-    # Consider appending /opt/X11/lib/pkgconfig to PKG_CONFIG_PATH and/or
-    # adding a symlink to /opt/X11/lib during build instead of depending on
-    # libx11/xorgproto.
-
-    # See also the "x11" method that was removed from brew recently:
-    # (Library/Homebrew/extend/os/mac/extend/ENV/std.rb)
-    # https://github.com/Homebrew/brew/commit/87dd13aea6691e9d5e0f3ba8d1d1f862a809212a#diff-ed4c0c77ea1a192666ae8a1758389fb65054649d71e0e39e14ee08e919fdb2b3L13-L34
-
-
-    args = %W[
-      --enable-osx-universal-binary=no
-      --prefix=#{prefix}
-      --disable-dependency-tracking
-      --disable-silent-rules
-      --disable-opencl
-      --enable-shared
-      --enable-static
-      --with-freetype=yes
-      --with-modules
-      --with-openjp2
-      --with-openexr
-      --with-webp=yes
-      --with-heic=yes
-      --with-gslib
-      --with-gs-font-dir=#{HOMEBREW_PREFIX}/share/ghostscript/fonts
-      --with-lqr
-      --without-fftw
-      --without-pango
-      --without-wmf
-      --enable-openmp
-      ac_cv_prog_c_openmp=-Xpreprocessor\ -fopenmp
-      ac_cv_prog_cxx_openmp=-Xpreprocessor\ -fopenmp
-      LDFLAGS=-lomp\ -lz
+    args = [
+      "--enable-osx-universal-binary=no",
+      "--prefix=#{prefix}",
+      "--disable-dependency-tracking",
+      "--disable-silent-rules",
+      "--disable-opencl",
+      "--enable-shared",
+      "--enable-static",
+      "--with-freetype=yes",
+      "--with-gvc=no",
+      "--with-modules",
+      "--with-openjp2",
+      "--with-openexr",
+      "--with-webp=yes",
+      "--with-heic=yes",
+      "--with-gslib",
+      "--with-gs-font-dir=#{HOMEBREW_PREFIX}/share/ghostscript/fonts",
+      "--with-lqr",
+      "--without-fftw",
+      "--without-pango",
+      "--without-wmf",
+      "--enable-openmp",
+      "ac_cv_prog_c_openmp=-Xpreprocessor -fopenmp",
+      "ac_cv_prog_cxx_openmp=-Xpreprocessor -fopenmp",
+      "LDFLAGS=-lomp -lz",
     ]
+
+    on_macos do
+      args << "--without-x"
+    end
 
     # versioned stuff in main tree is pointless for us
     inreplace "configure", "${PACKAGE_NAME}-${PACKAGE_BASE_VERSION}", "${PACKAGE_NAME}"
@@ -124,62 +89,3 @@ class Imagemagick < Formula
     assert_match "Helvetica", shell_output("#{bin}/identify -list font")
   end
 end
-
-
-# *EXPERIMENTAL PATCH*
-#
-# The ImageMagick configure script runs a number of compiler tests to check
-# for the existence of libraries such as libxext and libxt (-lXext -lXt).
-#
-# When the configure script is checking for XShmAttach in -lXext it does so
-# by testing that -lXext is present but without testing if
-# X11/extensions/XShm.h can be included.
-#
-# This check enables MAGICKCORE_HAVE_SHARED_MEMORY
-# This causes MagickCore/xwindow.c to include X11/extensions/XShm.h
-# This causes the `make install` step to fail
-#
-# As an experiment, the configure script is patched to test if
-# X11/extensions/XShm.h can be included.
-
-__END__
-diff --git a/configure b/configure
-index 662e288..4b4bc59 100755
---- a/configure
-+++ b/configure
-@@ -27835,6 +27835,8 @@ LIBS="-lICE $X_EXTRA_LIBS $LIBS"
- cat confdefs.h - <<_ACEOF >conftest.$ac_ext
- /* end confdefs.h.  */
-
-+#include <X11/ICE/ICElib.h>
-+
- /* Override any GCC internal prototype to avoid an error.
-    Use char because int might match the return type of a GCC
-    builtin and then its argument prototype would still apply.  */
-@@ -27931,6 +27933,8 @@ LIBS="-lXext  $LIBS"
- cat confdefs.h - <<_ACEOF >conftest.$ac_ext
- /* end confdefs.h.  */
- 
-+#include <X11/extensions/XShm.h>
-+
- /* Override any GCC internal prototype to avoid an error.
-    Use char because int might match the return type of a GCC
-    builtin and then its argument prototype would still apply.  */
-@@ -27978,6 +27982,8 @@ LIBS="-lXext  $LIBS"
- cat confdefs.h - <<_ACEOF >conftest.$ac_ext
- /* end confdefs.h.  */
- 
-+#include <X11/extensions/shape.h>
-+
- /* Override any GCC internal prototype to avoid an error.
-    Use char because int might match the return type of a GCC
-    builtin and then its argument prototype would still apply.  */
-@@ -28020,6 +28026,8 @@ LIBS="-lXt  $LIBS"
- cat confdefs.h - <<_ACEOF >conftest.$ac_ext
- /* end confdefs.h.  */
- 
-+#include <X11/Intrinsic.h>
-+
- /* Override any GCC internal prototype to avoid an error.
-    Use char because int might match the return type of a GCC
-    builtin and then its argument prototype would still apply.  */
