dc87629a78143963a87f0958058d70eafe132bf2
Thomas L. Kjeldsen <tlk@closureconsulting.com>
2026-01-01 18:24:27 +0100

    Interrim: Merge upstream but without X11 patch

--- Formula/imagemagick.rb
+++ Formula/imagemagick.rb
@@ -1,8 +1,8 @@
 class Imagemagick < Formula
   desc "Tools and libraries to manipulate images in many formats (X11 support)"
   homepage "https://imagemagick.org/index.php"
-  url "https://imagemagick.org/archive/releases/ImageMagick-7.1.2-11.tar.xz"
-  sha256 "9955090ece3fe283e98e175e2a22a23a61e92b16708e11bc69e068a5f4948f4a"
+  url "https://imagemagick.org/archive/releases/ImageMagick-7.1.2-12.tar.xz"
+  sha256 "e22c5dc6cd3f8e708a2809483fd10f8e37438ef7831ec8d3a07951ccd70eceba"
   license "ImageMagick"
   head "https://github.com/ImageMagick/ImageMagick.git", branch: "main"
 
@@ -13,6 +13,7 @@ class Imagemagick < Formula
 
 
   depends_on "pkgconf" => :build
+  depends_on "cairo"
   depends_on "fontconfig"
   depends_on "freetype"
   depends_on "jpeg-turbo"
@@ -21,6 +22,7 @@ class Imagemagick < Formula
   depends_on "liblqr"
   depends_on "libpng"
   depends_on "libraw"
+  depends_on "librsvg"
   depends_on "libtiff"
   depends_on "libtool"
   depends_on "libultrahdr"
@@ -36,6 +38,7 @@ class Imagemagick < Formula
   uses_from_macos "zlib"
 
   on_macos do
+    depends_on "gdk-pixbuf"
     depends_on "gettext"
     depends_on "glib"
     depends_on "imath"
@@ -43,22 +46,14 @@ class Imagemagick < Formula
   end
 
   on_linux do
+    depends_on "glib"
     depends_on "libx11"
     depends_on "libxext"
   end
 
   skip_clean :la
 
-  depends_on "libx11"
-  depends_on "graphviz" => :optional
-
-  patch :DATA
- 
   def install
-    # Add a symlink that points to the X11 include files provided by XQuartz.
-    # This prevents the `display wizard` command from segfaulting.
-    ln_s "/opt/X11/include/X11", "#{buildpath}/X11"
-
     # Avoid references to shim
     inreplace Dir["**/*-config.in"], "@PKG_CONFIG@", Formula["pkg-config"].opt_bin/"pkg-config"
     # versioned stuff in main tree is pointless for us
@@ -71,6 +66,8 @@ class Imagemagick < Formula
       "--enable-shared",
       "--enable-static",
       "--with-freetype=yes",
+      "--with-rsvg=yes",
+      "--with-gvc=no",
       "--with-modules",
       "--with-openjp2",
       "--with-openexr",
@@ -90,6 +87,7 @@ class Imagemagick < Formula
     ]
     if OS.mac?
       args += [
+        "--without-x",
         # Work around "checking for clang option to support OpenMP... unsupported"
         "ac_cv_prog_c_openmp=-Xpreprocessor -fopenmp",
         "ac_cv_prog_cxx_openmp=-Xpreprocessor -fopenmp",
@@ -113,7 +111,7 @@ class Imagemagick < Formula
 
     # Check support for recommended features and delegates.
     features = shell_output("#{bin}/magick -version")
-    %w[Modules freetype heic jpeg png raw tiff].each do |feature|
+    %w[Modules freetype heic jpeg png raw rsvg tiff].each do |feature|
       assert_match feature, features
     end
 
@@ -124,62 +122,3 @@ class Imagemagick < Formula
     end
   end
 end
-
-
-# *PATCH*
-#
-# The ImageMagick configure script runs a number of compiler tests to check
-# for the existence of libraries such as libxext and libxt (-lXext -lXt).
-#
-# When the configure script is checking for XShmAttach in -lXext it does so
-# by testing that -lXext is present but without testing if
-# X11/extensions/XShm.h can be included.
-#
-# This check enables MAGICKCORE_HAVE_SHARED_MEMORY
-# This causes MagickCore/xwindow.c to include X11/extensions/XShm.h
-# This causes the `make install` step to fail
-#
-# To work around this issue the configure script is patched to test if
-# X11/extensions/XShm.h can be included.
-
-__END__
-diff --git a/configure b/configure
-index 662e288..4b4bc59 100755
---- a/configure
-+++ b/configure
-@@ -29496,6 +29496,8 @@
- LIBS="-lICE $X_EXTRA_LIBS $LIBS"
- cat confdefs.h - <<_ACEOF >conftest.$ac_ext
- /* end confdefs.h.  */
-+
-+#include <X11/ICE/ICElib.h>
-
- /* Override any GCC internal prototype to avoid an error.
-    Use char because int might match the return type of a GCC
-@@ -29593,6 +29595,8 @@
- LIBS="-lXext  $LIBS"
- cat confdefs.h - <<_ACEOF >conftest.$ac_ext
- /* end confdefs.h.  */
-+
-+#include <X11/extensions/XShm.h>
-
- /* Override any GCC internal prototype to avoid an error.
-    Use char because int might match the return type of a GCC
-@@ -29640,6 +29644,8 @@
- LIBS="-lXext  $LIBS"
- cat confdefs.h - <<_ACEOF >conftest.$ac_ext
- /* end confdefs.h.  */
-+
-+#include <X11/extensions/shape.h>
-
- /* Override any GCC internal prototype to avoid an error.
-    Use char because int might match the return type of a GCC
-@@ -29682,6 +29688,8 @@
- LIBS="-lXt  $LIBS"
- cat confdefs.h - <<_ACEOF >conftest.$ac_ext
- /* end confdefs.h.  */
-+
-+#include <X11/Intrinsic.h>
-
- /* Override any GCC internal prototype to avoid an error.
-    Use char because int might match the return type of a GCC
